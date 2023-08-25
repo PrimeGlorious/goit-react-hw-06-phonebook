623074f54233b355d8840eac08dd3739b38b2cf3
PrimeGlorious <TheComboPunk@gmail.com>
2023-08-25 17:15:57 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+import { ContactsTitle } from './App.styled';
+
+function App() {
+  const contacts = useSelector(getContacts);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ContactsTitle>Phonebook</ContactsTitle>
+      <ContactForm />
+      <ContactsTitle>Contacts</ContactsTitle>
+      <Filter />
+      {contacts.length !== 0 && <ContactList />}
     </div>
   );
-};
+}
+
+export { App };
